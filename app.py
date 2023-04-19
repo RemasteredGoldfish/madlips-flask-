cbac718a124342f6787f204368e2fb18d6842ed9
Remastered Goldfish <99067800@mydavinci.nl>
2023-04-19 20:15:56 +0200

    madlips part 1 done

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def submit_form():
     examplequestion7 = request.form.get('examplequestion7')
 
     # Do some processing with the form data
-    result = f"The result of the form submission is: {examplequestion1}, {examplequestion2}, {examplequestion3}, {examplequestion4}, {examplequestion5}, {examplequestion6}, {examplequestion7}."
+    result = f"Er zijn veel mensen die niet kunnen {examplequestion1}. Neem nou {examplequestion2}. Zelfs met de hulp van een {examplequestion3} of zelfs {examplequestion4} kan {examplequestion2} niet {examplequestion1}. Dat heeft niet te maken met een gebrek aan {examplequestion5}, maar met een te veel aan {examplequestion6}. Te veel {examplequestion6} leidt tot {examplequestion7} en dat is niet goed als je wilt {examplequestion1}. Helaas voor {examplequestion2}"
 
     return render_template('result_form.html', result=result)
 app.run(debug=True)
