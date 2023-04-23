7b21ab5db5eb4aa84280be47dce4b237917d5468
Goldy <99067800@mydavinci.nl>
2023-04-23 03:44:29 +0200

    Im finally done

--- app.py
+++ app.py
@@ -34,8 +34,9 @@ def sumbit_formTwo():
     question5 = request.form.get('question5')
     question6 = request.form.get('question6')
     question7 = request.form.get('question7')
+    question8 = request.form.get('question8')
 
-    result = f"Er zijn veel mensen die niet kunnen {question1}. Neem nou {question2}. Zelfs met de hulp van een {question3} of zelfs {question4} kan {question2} niet {question1}. Dat heeft niet te maken met een gebrek aan {question5}, maar met een te veel aan {question6}. Te veel {question6} leidt tot {question7} en dat is niet goed als je wilt {question1}. Helaas voor {question2}"
+    result = f"Er heerst paniek in het koninkrijk {question1}. Koning Egmond is ten einde raad en als koning {question6} ten einde raad is, dan roept hij zijn ten-einde-raadsheer {question2}. /n'{question2}! Het is een ramp! het is een schande!' /n'Sire, Majesteit, Uwe Luidruchtigheid, wat is er aan de hand?' /n'Mijn {question1} is verdwenen! Zo maar, zonder waarschuwing. En ik had net {question5} voor hem gekocht!' /n'Majesteit, uw {question5} komt vast vanzelf weer terug?' /n'Ja da's leuk en aardig maar hoe moet ik in de tussentijd {question8} leren?' /n'Maar Sire, daar kunt u toch uw {question7} gebruiken.' /n'{question2}, je heb helemaal gelijk! Wat zou ik doen als ik jou niet had.' /n'{question4}, Sire.'"
 
     return render_template('result_formtwo.html', result=result)
-app.run()
+app.run(debug=True)
